36fee4749658a86b9cbc143a73da3c96c15fa07e
Salman Fakhri <salmanfakhri2@gmail.com>
2018-07-16 14:55:38 -0400

    start mvvm

---
+++ RutgersFood/ViewModel/FoodViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FoodViewModel.swift
+//  RutgersFood
+//
+//  Created by Salman Fakhri on 7/16/18.
+//  Copyright © 2018 Salman Fakhri. All rights reserved.
+//
+
+import Foundation
+
+class FoodViewModel {
+    
+    private let apiService: DataStore = {
+        let service = DataStore()
+        return service
+    }()
+    
+    init() {
+        
+    }
+}
